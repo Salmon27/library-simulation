4c0f1d57287c3082b54e303aa7f39253e33f9964
AJ Salmon <ajsalmoncrazyforacure@hotmail.com>
2020-02-06 13:51:15 -0600

    added some files

--- LibrarySimulation/src/librarysimulation/LibrarySimulation.java
+++ LibrarySimulation/src/librarysimulation/LibrarySimulation.java
@@ -1,7 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ AJ and Joey
+ CMPSC 222
+ 2/6/2020
+ This program is the main class of the LibrayrSimulation Project.
  */
 package librarysimulation;
 
