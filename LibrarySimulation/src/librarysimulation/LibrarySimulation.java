c461cd46fb5b0dc08c443e45b368afd36b2c4eeb
AJ Salmon <ajsalmoncrazyforacure@hotmail.com>
2020-02-18 13:01:11 -0600

    added features to main

--- LibrarySimulation/src/librarysimulation/LibrarySimulation.java
+++ LibrarySimulation/src/librarysimulation/LibrarySimulation.java
@@ -12,7 +12,9 @@ import java.util.regex.Pattern;
 
 public class LibrarySimulation {
 
-    private static Customer registerUser() throws Exception
+    private static final String DOMAINS[] = {"edu", "com", "net", "org"};
+
+    //private static Customer registerUser() throws Exception
     // This method prompts the user to enter Customer information, validates
     // it, and then returns the new Customer. If the customer quits at any time,
     // it throws an exception
@@ -24,89 +26,122 @@ public class LibrarySimulation {
         String email;
         String addr;
         String state;
-        Customer user;
-        
+        //Customer user;
+
         System.out.println("==============================");
         System.out.println("----  WELCOME, NEW USER   ----");
         System.out.println("==============================");
-        
-        while(flag) // name loop
+
+        while (flag) // name loop
         {
             System.out.println("Enter your name in 20 non-special characters or less: > ");
             name = console.nextLine().trim();
-            if (checkValidName(name))
+            if (checkValidName(name)) {
                 flag = false;
-            else
-            {
+            } else {
                 System.out.println("Not a valid name.");
                 name = "";
             }
         }
-        
+
         flag = true;
-        
-        while(flag) //phone loop
+
+        while (flag) //phone loop
         {
-            
+            System.out.println("Enter your phone number in the format XXX-XXXX: > ");
+            phone = console.nextLine().trim();
+            if (checkValidPhoneNumber(phone)) {
+                flag = false;
+            } else {
+                System.out.println("Not a valid phone number.");
+                name = "";
+            }
         }
-        
+
         flag = true;
-        
-        while(flag) //email loop
+
+        while (flag) //email loop
         {
-            
+
         }
-        
+
         flag = true;
-        
-        while(flag) //address loop
+
+        while (flag) //address loop
         {
-            
+
         }
-        
+
         flag = true;
-        
-        while(flag) //state loop
+
+        while (flag) //state loop
         {
-            
+
         }
-        
-  
+
     }
-    
-    
-    private static boolean checkValidPhoneNumber(String phone)
-    {
-        Pattern phoneNumber = Pattern.compile("\\d{3}-\\d{7}");
+
+    private static boolean checkValidPhoneNumber(String phone) {
+        Pattern phoneNumber = Pattern.compile("(\\d{3})-(\\d{7})");
         Matcher correctPhoneNumber = phoneNumber.matcher(phone);
-        
-        if (correctPhoneNumber.find())
-        {
+
+        if (correctPhoneNumber.find()) {
             return true;
         }
-        
+
         return false;
     }
-    
-    
+
     private static boolean checkValidName(String name) // checks name if under 20
-                                                       // and no special chars
+    // and no special chars
     {
-        if (name.length() < 21) 
-        {
-            Pattern specialChars = Pattern.compile ("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+        if (name.length() < 21 && name.length() >= 1) {
+            Pattern specialChars = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
             Matcher hasSpecialChars = specialChars.matcher(name);
-            
-            if(hasSpecialChars.find())
+
+            if (hasSpecialChars.find()) {
                 return false;
-            else
+            } else {
                 return true;
+            }
         }
         return false;
     }
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private static boolean checkValidEmail(String email, String name) //checks if starts with recipients name < 8 char  then @. 5 char domain
+    // then top lvl domain
+    {
+
+        Pattern emailPattern = Pattern.compile("^(.{1,8})@([A-Z, a-z]{1,5})\\.([a-z]{3})");
+        Matcher correctEmail = emailPattern.matcher(email);
+
+        if (name.length() > 8) {
+            name = name.substring(0, 7);
+        }
+        if (email.startsWith(name)) {
+            for (int i = 0; i < DOMAINS.length; i++) {
+                if (email.endsWith(DOMAINS[i])) {
+                    if (correctEmail.find()) {
+                        return true;
+                    }
+                    else
+                    {
+                        System.out.println("Invalid email.");
+                        return false;
+                    }
+                }
+            }
+            System.out.println("Invalid top level domain. Must be: .edu, .com, .net, or .org");
+        }
+        System.out.println("Email address does not start with your name.");
+        return false;
     }
-    
+
+    public static void main(String[] args) {
+        String test = "\\.27@hotm.edw";
+
+        System.out.println(checkValidEmail(test, "Aaron"));
+
+    }
+
 }
